303fdf333d22bc21f052b8f5f9ba3cff635c26c3
qvil <qvil1127@gmail.com>
2018-01-20 21:28:16 +0900

    Add todo-list

---
+++ README.md
@@ -0,0 +1,5 @@
+## Q
+1. 왜 script load를 body 태그의 끝에 하면 동작하고 head태그나 body태그의 위에 선언하면 vue가 동작하지 않는가?
+>스크립트가 실행되는 시점에 태그가 브라우저에 로드되지않아서그래용 body영역이 읽혀야 DOM 객체가 만들어지는데 그전에 스크립트가 로드되서 그렇죠 - 카톡 오픈채팅 vuue님 -
+1. Not showing dev tools vue panel
+>
--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        msg: 'Hello'
+Vue.config.devtools = true;
+
+Vue.component("todo-item", {
+  template: "\
+    <li>\
+      {{ title }}\
+      <button v-on:click=\"$emit(\'remove\')\">X</button>\
+    </li>/\
+  ",
+  props: ['title']
+});
+
+new Vue({
+  el: '#todo-list-example',
+  data: {
+    newTodoText: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Talk',
+      },
+      {
+        id: 2,
+        title: 'Play',
+      },
+      {
+        id: 3,
+        title: 'Love',
+      },
+    ],
+    nextTodoId: 4,
+  },
+  methods: {
+    addNewTodo: function() {
+      this.todos.push({
+        id: this.newTodoId++,
+        title: this.newTodoText,
+      })
+      this.newTodoText = ''
     }
+  }
 })
